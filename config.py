2b890a51bee0abbf2f9999355402f1aa41e0b7b3
avKostanov <avkostanov@gmail.com>
2021-06-21 17:05:19 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,6 @@ import torch
 
 # training
 DEVICE = torch.device('cuda') if torch.cuda.is_available() else 'cpu'
-NUM_EPOCHS = 50
-VALID_SIZE = 0.8
-LOSS_FUNCTION = torch.nn.L1Loss()
 
 # paths
 PATH_TO_TEST_DATASET = '/home/alexey/vanishing_point/test'
